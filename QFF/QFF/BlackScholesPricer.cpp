7ec5fa7e43e8a1557a628deb55c13f5d7daedd44
Kevin Lee <kevin_yiu21@hotmail.com>
2018-07-18 11:35:14 +1000

    Commented out the implementations in Black76Pricer and BlackScholesPricer

--- QFF/QFF/BlackScholesPricer.cpp
+++ QFF/QFF/BlackScholesPricer.cpp
@@ -3,12 +3,14 @@
 
 double BlackScholesPricer::evaluate(const IMarketData& marketData, const BasicTradeInfo& basicTradeInfo, string optionType, const ICurrency& currency) const
 { 	
-	return BlackScholesFormula(
-		marketData.getSpot(basicTradeInfo.underlying),
-		basicTradeInfo.strike,
-		marketData.getZeroRate("AUD_STD", basicTradeInfo.expiryDate), 
-		basicTradeInfo.timeToMaturity,
-		marketData.getVolatility(basicTradeInfo.underlying, basicTradeInfo.expiryDate, basicTradeInfo.strike),
-		marketData.getDividendYieldEstimate(basicTradeInfo.underlying, basicTradeInfo.expiryDate),
-		optionType);
+	//TODO: Make BlackScholesFormula takes discount factor instead of zero rate and time to maturity. 
+	//return BlackScholesFormula(
+	//	marketData.getSpot(basicTradeInfo.underlying),
+	//	basicTradeInfo.strike,
+	//	marketData.getZeroRate("AUD_STD", basicTradeInfo.expiryDate), 
+	//	basicTradeInfo.timeToMaturity,
+	//	marketData.getVolatility(basicTradeInfo.underlying, basicTradeInfo.expiryDate, basicTradeInfo.strike),
+	//	marketData.getDividendYieldEstimate(basicTradeInfo.underlying, basicTradeInfo.expiryDate),
+	//	optionType);
+	return 0;
 }

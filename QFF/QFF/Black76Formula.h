21273f8f3b57cf9ad5975e6b67219f2a04c47ace
Kevin Lee <kevin_yiu21@hotmail.com>
2020-03-23 09:41:33 +1100

    Included Dlib library and added BlackScholesImpliedVolatility function.

--- QFF/QFF/Black76Formula.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string_view>
-
-namespace qff {
-double Black76Formula(double forward_price, double strike,
-                      double discount_factor, double time_to_maturity,
-                      double volatility, std::string_view option_type);
-}  // namespace qff
--- QFF/QFF_AAD/BlackScholesFormula.h
+++ QFF/QFF_AAD/BlackScholesFormula.h
@@ -1,10 +1,12 @@
 #pragma once
+
 #include <boost/math/distributions/normal.hpp>
 #include <cmath>
 #include <stdexcept>
 #include <string_view>
 
 #include "../AAD/aad_all.h"
+#include "../include/dlib/source/dlib/optimization.h"
 
 namespace qff_a {
 template <typename T>
@@ -26,5 +28,27 @@ auto BlackScholesFormula(T spot, double strike, T discount_factor,
              spot * aad::normal_cdf(-d1));
   }
   throw std::invalid_argument("invalid option type");
+}
+
+inline double BlackScholesImpliedVolatility(double premium, double spot,
+                                            double strike,
+                                            double discount_factor,
+                                            double time_to_maturity,
+                                            std::string_view option_type) {
+  // Initial guess
+  dlib::matrix<double, 0, 1> vol = {0.5};
+  auto search = dlib::bfgs_search_strategy();
+  auto stop = dlib::objective_delta_stop_strategy(1e-7);
+
+  auto f = [&](const dlib::matrix<double, 0, 1>& input) {
+    return pow((BlackScholesFormula(spot, strike, discount_factor,
+                                    time_to_maturity, input(0), option_type) -
+                premium),
+               2);
+  };
+
+  find_min_using_approximate_derivatives(search, stop, f, vol, 0.0);
+
+  return vol(0);
 }
 }  // namespace qff_a
